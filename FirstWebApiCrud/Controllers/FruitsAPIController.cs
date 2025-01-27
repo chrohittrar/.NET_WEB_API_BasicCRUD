596ac922b6bb68b02e5a3b6ffab6c044811417ef
Rohit Kumar <chrohittrar@gmail.com>
2025-01-27 14:52:38 +0530

    Basic WebAPI project using Hardcoded values

---
+++ FirstWebApiCrud/Controllers/FruitsAPIController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApiCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsAPIController : ControllerBase
+    {
+        public List<string> fruits = new List<string>()
+        {
+            "Apple",
+            "Banana",
+            "Orange",
+            "Mango",
+            "Grapes"
+        };
+
+        [HttpGet]
+        public List<string> GetFruits()
+        {
+            return fruits;
+        }
+
+        [HttpGet("{id}")]
+        public string GetFruits(int id)
+        { 
+            return fruits.ElementAt(id);
+        }
+    }
+}
